486e6af584806279e71af4614bbce7ce2fc34c5d
Diego Rodrigo Manrique Avila <diegorodavila2003@gmail.com>
2025-10-31 14:41:06 -0500

    Update Configuracion.jsx

--- src/pages/admin/Configuracion/Configuracion.jsx
+++ src/pages/admin/Configuracion/Configuracion.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../admin/styles.css";
-import "../admin/panel.css";
+import "../style.css";
+import "../panel.css";
 
 const Configuracion = () => {
   return (
