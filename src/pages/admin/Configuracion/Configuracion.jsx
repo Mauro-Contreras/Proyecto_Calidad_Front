044cb34f8658fea8e67dadc672d78bf92d808e9a
Jair1811 <jairmla018@gmail.com>
2025-11-01 13:48:36 -0500

    Update Configuracion.jsx

--- src/pages/admin/Configuracion/Configuracion.jsx
+++ src/pages/admin/Configuracion/Configuracion.jsx
@@ -5,16 +5,35 @@ import "../panel.css";
 
 const Configuracion = () => {
   return (
-    <div className="sb-nav-fixed">
+      <div className="sb-nav-fixed">
       {/* NAVBAR SUPERIOR */}
       <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-        <a className="navbar-brand ps-3" href="/index">DREAM CARS</a>
+        <a className="navbar-brand ps-3" href="#">
+          DREAM CARS
+        </a>
         <button
           className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0"
           id="sidebarToggle"
         >
           <i className="fas fa-bars"></i>
         </button>
+
+        {/* BUSCADOR */}
+        <form className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+          <div className="input-group">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Buscar..."
+              aria-label="Search"
+            />
+            <button className="btn btn-primary" type="button">
+              <i className="fas fa-search"></i>
+            </button>
+          </div>
+        </form>
+
+        {/* USUARIO */}
         <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
           <li className="nav-item dropdown">
             <a
@@ -27,11 +46,28 @@ const Configuracion = () => {
             >
               <i className="fas fa-user fa-fw"></i>
             </a>
-            <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-              <li><a className="dropdown-item" href="#">Configuraciones</a></li>
-              <li><a className="dropdown-item" href="#">Registro de Actividad</a></li>
-              <li><hr className="dropdown-divider" /></li>
-              <li><a className="dropdown-item" href="#">Cerrar Sesión</a></li>
+            <ul
+              className="dropdown-menu dropdown-menu-end"
+              aria-labelledby="navbarDropdown"
+            >
+              <li>
+                <a className="dropdown-item" href="#">
+                  Configuraciones
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Registro de Actividad
+                </a>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+              <li>
+                <a className="dropdown-item" href="/login">
+                  Cerrar Sesión
+                </a>
+              </li>
             </ul>
           </li>
         </ul>
