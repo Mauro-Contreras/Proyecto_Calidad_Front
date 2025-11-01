c0cb122624c28d5861f3cef717bb6e163b34fe34
Jair1811 <jairmla018@gmail.com>
2025-11-01 13:48:50 -0500

    Update OrdenNueva.jsx

--- src/pages/admin/OrdenesTrabajo/OrdenNueva.jsx
+++ src/pages/admin/OrdenesTrabajo/OrdenNueva.jsx
@@ -35,38 +35,58 @@ function OrdenNueva() {
   };
 
   return (
-    <div className="sb-nav-fixed">
-      {/* Navbar superior */}
+      <div className="sb-nav-fixed">
+      {/* NAVBAR SUPERIOR */}
       <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-        <Link className="navbar-brand ps-3" to="/admin">
+        <a className="navbar-brand ps-3" href="#">
           DREAM CARS
-        </Link>
+        </a>
         <button
           className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0"
           id="sidebarToggle"
         >
           <i className="fas fa-bars"></i>
         </button>
+
+        {/* BUSCADOR */}
+        <form className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+          <div className="input-group">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Buscar..."
+              aria-label="Search"
+            />
+            <button className="btn btn-primary" type="button">
+              <i className="fas fa-search"></i>
+            </button>
+          </div>
+        </form>
+
+        {/* USUARIO */}
         <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
           <li className="nav-item dropdown">
             <a
               className="nav-link dropdown-toggle"
               id="navbarDropdown"
-              href="#!"
+              href="#"
               role="button"
               data-bs-toggle="dropdown"
               aria-expanded="false"
             >
               <i className="fas fa-user fa-fw"></i>
             </a>
-            <ul className="dropdown-menu dropdown-menu-end">
+            <ul
+              className="dropdown-menu dropdown-menu-end"
+              aria-labelledby="navbarDropdown"
+            >
               <li>
-                <a className="dropdown-item" href="#!">
+                <a className="dropdown-item" href="#">
                   Configuraciones
                 </a>
               </li>
               <li>
-                <a className="dropdown-item" href="#!">
+                <a className="dropdown-item" href="#">
                   Registro de Actividad
                 </a>
               </li>
@@ -74,7 +94,7 @@ function OrdenNueva() {
                 <hr className="dropdown-divider" />
               </li>
               <li>
-                <a className="dropdown-item" href="#!">
+                <a className="dropdown-item" href="/login">
                   Cerrar Sesión
                 </a>
               </li>
