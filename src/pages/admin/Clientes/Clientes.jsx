42abbac69c8781b13efa8131d2ed93483fc49b4e
Jair1811 <jairmla018@gmail.com>
2025-11-01 13:48:31 -0500

    Update Clientes.jsx

--- src/pages/admin/Clientes/Clientes.jsx
+++ src/pages/admin/Clientes/Clientes.jsx
@@ -50,23 +50,69 @@ export default function Clientes() {
   };
 
   return (
-    <div className="sb-nav-fixed">
+  <div className="sb-nav-fixed">
       {/* NAVBAR SUPERIOR */}
       <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-        <a className="navbar-brand ps-3" href="/admin">DREAM CARS</a>
-        <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle">
+        <a className="navbar-brand ps-3" href="#">
+          DREAM CARS
+        </a>
+        <button
+          className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0"
+          id="sidebarToggle"
+        >
           <i className="fas fa-bars"></i>
         </button>
+
+        {/* BUSCADOR */}
+        <form className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+          <div className="input-group">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Buscar..."
+              aria-label="Search"
+            />
+            <button className="btn btn-primary" type="button">
+              <i className="fas fa-search"></i>
+            </button>
+          </div>
+        </form>
+
+        {/* USUARIO */}
         <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
           <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a
+              className="nav-link dropdown-toggle"
+              id="navbarDropdown"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
               <i className="fas fa-user fa-fw"></i>
             </a>
-            <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-              <li><a className="dropdown-item" href="#">Configuraciones</a></li>
-              <li><a className="dropdown-item" href="#">Registro de Actividad</a></li>
-              <li><hr className="dropdown-divider" /></li>
-              <li><a className="dropdown-item" href="#">Cerrar Sesión</a></li>
+            <ul
+              className="dropdown-menu dropdown-menu-end"
+              aria-labelledby="navbarDropdown"
+            >
+              <li>
+                <a className="dropdown-item" href="#">
+                  Configuraciones
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Registro de Actividad
+                </a>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+              <li>
+                <a className="dropdown-item" href="/login">
+                  Cerrar Sesión
+                </a>
+              </li>
             </ul>
           </li>
         </ul>
@@ -78,7 +124,7 @@ export default function Clientes() {
           <nav className="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
             <div className="sb-sidenav-menu">
               <div className="nav">
-                <a className="nav-link" href="/admin">
+                <a className="nav-link" href="/admin/index">
                   <div className="sb-nav-link-icon"><i className="fas fa-tachometer-alt"></i></div>
                   Dashboard
                 </a>
