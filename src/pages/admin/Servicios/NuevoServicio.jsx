c2e247464cc03687d573cbe8ff85f91903658821
Diego Rodrigo Manrique Avila <diegorodavila2003@gmail.com>
2025-11-02 19:56:13 -0500

    Update NuevoServicio.jsx

--- src/pages/admin/Servicios/NuevoServicio.jsx
+++ src/pages/admin/Servicios/NuevoServicio.jsx
@@ -245,6 +245,20 @@ const NuevoServicio = () => {
                   </nav>
                 </div>
 
+                <Link className="nav-link" to="/admin/facturacion">
+                 <div className="sb-nav-link-icon">
+                 <i className="fa-solid fa-file-invoice-dollar"></i>
+                 </div>
+                  Facturación
+                </Link>
+                                
+                <Link className="nav-link" to="/admin/inventario">
+                  <div className="sb-nav-link-icon">
+                  <i className="fa-solid fa-boxes-stacked"></i>
+                  </div>
+                  Inventario
+                </Link>
+
                 <a className="nav-link" href="/admin/reportes">
                   <div className="sb-nav-link-icon">
                     <i className="fas fa-chart-line"></i>
