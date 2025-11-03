6b8db5b964503865aeb758d9a16bb8be4b6eb4b3
Diego Rodrigo Manrique Avila <diegorodavila2003@gmail.com>
2025-11-02 20:09:50 -0500

    Update Vehiculos.jsx

--- src/pages/admin/Vehiculos/Vehiculos.jsx
+++ src/pages/admin/Vehiculos/Vehiculos.jsx
@@ -229,6 +229,20 @@ const Vehiculos = () => {
                   </nav>
                 </div>
 
+                <Link className="nav-link" to="/admin/facturacion">
+                  <div className="sb-nav-link-icon">
+                  <i className="fa-solid fa-file-invoice-dollar"></i>
+                  </div>
+                  Facturación
+                </Link>
+                                
+                <Link className="nav-link" to="/admin/inventario">
+                  <div className="sb-nav-link-icon">
+                  <i className="fa-solid fa-boxes-stacked"></i>
+                  </div>
+                  Inventario
+                </Link>
+
                 {/* REPORTES */}
                 <a className="nav-link" href="/admin/reportes">
                   <div className="sb-nav-link-icon">
